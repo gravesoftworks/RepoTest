02e46f50882126567d10292873ac6dc000e00046
Joshua Taylor <jdtaylor530@yahoo.com>
2018-10-26 17:35:37 -0400

    Changed some stuff.

--- RepoTest/main.cpp
+++ RepoTest/main.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	std::cout << "This is a test!";
-
+	std::cout << "This is a test!" << std::endl << "Added this line for a quick test of this repo thing" << std::endl;
+	system("pause");
 	return 0;
+
 }
